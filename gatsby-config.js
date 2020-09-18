fbbaf2a73cb6df5ddada5ac14419bd8cd85ad526
Tushar Joshi <tushar.jo@endurance.com>
2020-09-18 18:24:29 +0530

    Key update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,21 +2,22 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    siteName: 'My Shop',
+    siteName: "My Shop",
   },
   plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-datocms`,
-      options: { apiToken: process.env.DATO_API_TOKEN },
+      options: { apiToken: `0a7d800f96ba6a135f35ab6b52170e` },
     },
     {
-      resolve: 'gatsby-plugin-snipcart',
+      resolve: "gatsby-plugin-snipcart",
       options: {
-        apiKey: 'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
-        autopop: true
-      }
+        apiKey:
+          "YTdkNjU2ZDQtNjg5ZS00MmFmLWFlZjItNDk1ZDdmOGRjNTg5NjM3MzYwMTMyNTMxNzEwNDk4",
+        autopop: true,
+      },
     },
   ],
-}
+};
